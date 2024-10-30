518e9fbbe8eec30e06608d62d4514b5e93de4d0c
JuanGuillermoPantoja <jgpantoja13@gmail.com>
2024-10-30 08:31:38 -0500

    fixFooterV0.2

--- src/components/SectionHome.jsx
+++ src/components/SectionHome.jsx
@@ -3,12 +3,12 @@ import imgMazorcaCacao from '/images/grano-cacao.jpg'
 const sectionHome = () => {
   return (
     <>
-      <section className="h-[600px] w-full flex items-center justify-center">
-        <div className="w-[90%] flex justify-center items-center flex-col">
-          <h1 className="text-3xl my-4 font-bold md:text-5xl">
+      <section className="h-[800px] w-full flex items-start justify-center">
+        <div className="w-[90%] h-full flex justify-center items-center flex-col">
+          <h1 className="text-3xl font-bold md:text-5xl mb-4">
             Coqui<span id='spanTitle' className="text-[#41b93b] font-bold">Cacao</span>
           </h1>
-          <p className="w-[80%] text-center md:w-3/5">
+          <p className="w-[80%] my-4 text-center md:w-3/5">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
             laboriosam unde nesciunt veritatis perferendis vel nihil assumenda
             corrupti nam esse vitae reprehenderit, reiciendis iste ipsum,
