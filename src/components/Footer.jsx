96e6d830947d9727c2d4234861132f8a16eab15f
JuanGuillermoPantoja <jgpantoja13@gmail.com>
2024-10-30 09:11:20 -0500

    updateSectionHome_and_historyCacaov0.1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <div className=" p-4 border-t-[1px] h-[50px] rounded-t-full text-center content-center bg-[#41b93b] border-gray-200">
+      <div className=" p-4 border-t-[1px] h-[60px] rounded-t-full text-center content-center bg-[#41b93b] border-gray-200 overflow-hidden">
         <p className="text-gray-200">
           © todos los derechos reservados por SENNOVA
         </p>
--- src/components/SectionHome.jsx
+++ src/components/SectionHome.jsx
@@ -3,9 +3,9 @@ import imgMazorcaCacao from '/images/grano-cacao.jpg'
 const sectionHome = () => {
   return (
     <>
-      <section className="h-[800px] w-full flex items-start justify-center">
-        <div className="w-[90%] h-full flex justify-center items-center flex-col gap-8">
-          <h1 className="text-3xl font-bold md:text-5xl ">
+      <section className="w-full flex items-start justify-center overflow-hidden">
+        <div className="w-[90%] h-full flex justify-center items-center flex-col gap-4  ">
+          <h1 className="text-3xl font-bold md:text-5xl mt-4">
             Coqui<span id='spanTitle' className="text-[#41b93b] font-bold">Cacao</span>
           </h1>
           <p className="w-[80%] text-center md:w-3/5">
@@ -14,7 +14,7 @@ const sectionHome = () => {
             corrupti nam esse vitae reprehenderit, reiciendis iste ipsum,
             voluptates harum, consequuntur dignissimos! Fugiat.
           </p>
-          <img className='w-1/2 mt-4 border-2 border-gray-100 rounded-full md:w-1/4' src={imgMazorcaCacao} alt="" />
+          <img className='w-1/2 border-2 mb-4 border-gray-100 rounded-full md:w-1/4' src={imgMazorcaCacao} alt="" />
         </div>
       </section>
     </>
