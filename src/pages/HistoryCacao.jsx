ace147f63ff97e96b8eeb829529e3021925b072c
JuanGuillermoPantoja <jgpantoja13@gmail.com>
2024-12-21 08:41:45 -0500

    text-justify_history_cacao

--- src/pages/HistoryCacao.jsx
+++ src/pages/HistoryCacao.jsx
@@ -11,14 +11,14 @@ const HistoryCacao = () => {
           <h2 className="text-2xl lg:text-4xl lg:font-bold my-4">
             Historia del cacao
           </h2>
-          <p className="mb-4 lg:w-3/5">
+          <p className="text-justify mb-4 lg:w-3/5">
             Desde la Conquista y la Colonia española ha existido la idea
             generalizada de que el cacao es oriundo de Méjico y que fue de allí
             donde se desperdigó hacia el sur del continente; incluso era parte
             de la narrativa, que los españoles lo habían dispersado para su
             cultivo en Suramérica.
           </p>
-          <p className="mb-4 lg:w-3/5">
+          <p className="text-justify mb-4 lg:w-3/5">
             los españoles lo habían dispersado para su cultivo en Suramérica.
             Pero desde mediados del siglo XX, gracias a la investigación
             botánica en la región amazónica, comienza a ponerse en duda tal
@@ -27,13 +27,13 @@ const HistoryCacao = () => {
             torno a él.
           </p>
           <img className="w-1/2 lg:w-1/3" src={historyCacao} alt="" />
-          <p className="mb-4 lg:w-3/5">
+          <p className="text-justify mb-4 lg:w-3/5">
             Hasta que, en el 2012, un grupo de arqueólogos franco-ecuatorianos,
             que trabajan desde comienzos del nuevo siglo en el sitio Santa
             Ana-La Florida, ubicado en el cantón Palanda, en la provincia de
             Zamora Chinchipe, en Ecuador, cambiaron la historia.
           </p>
-          <p className="mb-4 lg:w-3/5">
+          <p className="text-justify mb-4 lg:w-3/5">
             En tal yacimiento arqueológico se obtiene evidencia de basureros
             domésticos y de ofrendas funerarias, de donde se obtuvieron semillas
             y residuos de bebidas de uso cotidiano, con varias fechas de Carbono
